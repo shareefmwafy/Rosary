300ee5dc427cbc61c42bf75be9fa0aa4efd57b76
shareef <s12029160@stu.najah.edu>
2024-02-11 22:58:05 +0200

    add anotherbutton

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 const button = document.querySelectorAll("button");
-
+const zero = document.querySelectorAll(".zero");
 
 let count =0;
 for(let i =0 ;i <button.length; i++){
     button[i].onclick = function(event) {
         event.preventDefault();
         let span = button[i].previousElementSibling;
-        console.log(span);
+        // console.log(span);
          count = parseInt(span.innerHTML) + 1;
          span.innerHTML = count;
 
     }
-    
+}
+for (let i=0; i<zero.length; i++) {
+    zero[i].onclick = function(event) {
+        event.preventDefault();
+        let span = zero[i].previousElementSibling;
+        let span1 =span.previousElementSibling;
+        span1.innerHTML =0;
+
+    }
 
 }
-
 
